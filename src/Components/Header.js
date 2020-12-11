aed8d442e2caccbacfc9de7246efcf72f9bf6e1a
KelsCree <kelseycreehan@gmail.com>
2020-12-11 16:12:37 -0700

    Create react app and start header component.

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return(
+      <div className="header">
+        <h1 id="titleText">Hi, I'm Kelsey.</h1>
+      </div>
+
+
+  )
+}
