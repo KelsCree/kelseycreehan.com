84fc761a50e9c97cd02386c140c21d5d06c399a3
KelsCree <kelseycreehan@gmail.com>
2020-12-13 16:59:32 -0700

    Add about component.

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+
+
+  return (
+    <div id="About">
+      <h1>About</h1>
+      <p> Ambitous software developer and communication enthusiast.
+        Former public relations professional who discovered a love for building quality applications.
+        My career hasn't been a straight line, but my 5+ years of project management
+        I see the glass half full and am usually laughing at something. 
+      </p>
+    </div>
+  )
+}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default function Header() {
   return(
+    <>
       <div className="header">
         <h1 id="titleText">Hi, I'm Kelsey.</h1>
         <h3 id="subTitle">Full-Stack Software Engineer</h3>
       </div>
+    </>
   )
 }
