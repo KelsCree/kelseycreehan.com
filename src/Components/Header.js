9c0090d06e1ca73b5063f1a45085044fc16d5ec0
KelsCree <kelseycreehan@gmail.com>
2020-12-13 15:18:39 -0700

    Create responsive header title.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,7 @@ export default function Header() {
   return(
       <div className="header">
         <h1 id="titleText">Hi, I'm Kelsey.</h1>
+        <h3 id="subTitle">Full-Stack Software Engineer</h3>
       </div>
-
-
   )
 }
