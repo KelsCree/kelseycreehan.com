7532b0ae1de086e1ceb0795e9d80193d37129e24
KelsCree <kelseycreehan@gmail.com>
2020-12-21 15:23:39 -0500

    Redesign intro component.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
   return(
     <>
       <div className="header">
-        <h1 id="titleText">Hi, I'm Kelsey.</h1>
-        <h3 id="subTitle">Full-Stack Software Engineer</h3>
+        <div className="upper-header">
+          <h1 id="titleText">Hi, I'm Kelsey.</h1>
+          <h3 id="subTitle">Full-Stack Software Engineer</h3>
+          <section id="menu">
+            <button id="about-button">ABOUT</button>
+            <button id="projects-button">PROJECTS</button>
+            <button id="experience-button">EXPERIENCE</button>
+            <button id="contact-button">CONTACT</button>
+          </section>
+        </div>
+        <section id="down-arrow">
+          <FontAwesomeIcon icon={faAngleDoubleDown} size="3x" />
+        </section>
       </div>
     </>
   )
